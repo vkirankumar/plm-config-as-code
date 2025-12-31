3db3bb857c3e5344f1aea309d0edc777985fdab1
vkirankumar <rockiran.2525@gmail.com>
2025-12-31 15:33:22 +0100

    Added liquibase config for generate, update and diff changelogs

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# plm-config-as-code
-Liquibase node app to transform PLM catalogue in Postgres RDBMS to config as code
+# plm-transform
+Liquibase node app for plm catalog as config as code
+
+#liquibase diffChangeLog --diffTypes=data,table,column,primaryKey,index,foreignKey,uniqueConstraint  --changelogFile=./change-diff.yaml
+#liquibase diff --diff-types=data,column,table
+#liquibase update --changelogFile=./change-diff.yaml
+#liquibase tag-exists --tag=v1.0.0
+#liquibase generateChangeLog --diffTypes=data,table,column,primaryKey,index,foreignKey,uniqueConstraint  --changelogFile=./change-log-master.yaml
---
+++ src/index.ts
@@ -0,0 +1,103 @@
+import { log } from 'console';
+import {
+    Liquibase,
+    POSTGRESQL_DEFAULT_CONFIG
+} from 'liquibase';
+import fs from 'fs';
+
+const args = process.argv.slice(2);
+
+// const config_dev = {
+//     ...POSTGRESQL_DEFAULT_CONFIG,
+//     password: 'postgres',
+//     url: 'jdbc:postgresql://localhost:4001/postgres',
+// };
+
+// const config_prod = {
+//     ...POSTGRESQL_DEFAULT_CONFIG,
+//     changeLogFile: './changelog-data-1.xml',
+//     password: 'postgres',
+//     url: 'jdbc:postgresql://localhost:4001/prod',
+// };
+
+const diffTypes: string = "data,table,column,primaryKey,index,foreignKey,uniqueConstraint";
+// const referenceParam: string = ' --referenceUrl=jdbc:postgresql://localhost:4001/preprod --referenceUsername=postgres referencePassword=postgres';
+// const liquibase_dev: Liquibase = new Liquibase(config_dev);
+// const liquibase_prod: Liquibase = new Liquibase(config_prod);
+const timeStamp: string  = new Date().toISOString().replace(/[:.&]/g, "-");
+
+const init = async () => {
+    log("Hello Liquibase!!!");
+    try {
+        // generateChangeLog();
+        // diff();
+        // update();
+        // diffChangeLog();
+        log("Completed!!");
+    } catch (error) {
+        log("Failed with error " + error);
+    }
+}
+
+const generateChangeLog = (): void => {
+    const config_dev = {
+        ...POSTGRESQL_DEFAULT_CONFIG,
+        password: 'postgres',
+        url: 'jdbc:postgresql://localhost:4001/postgres',
+    };
+    const liquibase: Liquibase = new Liquibase(config_dev);
+    liquibase.generateChangeLog({diffTypes,
+             changelogFile: `./db/master/changelog-master-${timeStamp}.yaml`});
+}
+
+const diff = async () => {
+    const config = {
+        ...POSTGRESQL_DEFAULT_CONFIG,
+        password: 'postgres',
+        url: 'jdbc:postgresql://localhost:4001/prod',
+        referenceUrl: 'jdbc:postgresql://localhost:4001/dev',
+        referenceUsername: 'postgres',
+        referencePassword: 'postgres',
+        schemas: 'public',
+        referenceSchemas: 'public'
+    };
+    const liquibase: Liquibase = new Liquibase(config);
+    const diff: string = await liquibase.diff({diffTypes});
+    log(diff);
+}
+
+const update = (): void => {
+    const config = {
+        ...POSTGRESQL_DEFAULT_CONFIG,
+        password: 'npg_cweS1VpKl0JL',
+        username: 'neondb_owner',
+        url: 'jdbc:postgresql://ep-summer-hill-ad1oha2r-pooler.c-2.us-east-1.aws.neon.tech:5432/prod',
+        changeLogFile: './db/master/changelog-master-2025-12-30T09-07-24-508Z.yaml',
+    };
+    const liquibase: Liquibase = new Liquibase(config);
+    liquibase.update({});
+}
+
+const diffChangeLog = async () => {
+    const fileName = `changelog-diff-${timeStamp}.yaml`;
+    const config = {
+        ...POSTGRESQL_DEFAULT_CONFIG,
+        password: 'postgres',
+        url: 'jdbc:postgresql://localhost:4001/prod',
+        referenceUrl: 'jdbc:postgresql://localhost:4001/dev',
+        referenceUsername: 'postgres',
+        referencePassword: 'postgres',
+        schemas: 'public',
+        referenceSchemas: 'public',
+        changeLogFile: `./db/diff/${fileName}`,
+    };
+    const liquibase: Liquibase = new Liquibase(config);
+    await liquibase.diffChangelog({diffTypes});
+    if (fs.existsSync(`./db/diff/${fileName}`)) {
+        log("Diff generated successfully!!!");
+    } else {
+        log("Schemas are equal");
+    }
+}
+
+init();
