f56c72c5a9cbfc71d5c4353fb26dcffe86783eb7
vkirankumar <rockiran.2525@gmail.com>
2026-01-05 20:18:23 +0100

    Test .env with workflow.

--- src/test.ts
+++ src/test.ts
@@ -7,17 +7,17 @@ const init = async () => {
     log("Hello Liquibase!!!");
     try {
         let str = process.env.DB_USERNAME;
-        log("Username = " + str?.substring(0, str.length - 1));
+        log("Username = " + str?.substring(0, str.length));
         str = process.env.DB_PASSWORD;
-        log("Password = " + str?.substring(0, str.length - 1));
+        log("Password = " + str?.substring(0, str.length));
         str = process.env.PORT;
-        log("Post = " + str?.substring(0, str.length - 1));
+        log("Post = " + str?.substring(0, str.length));
         str = process.env.DB_HOST;
-        log("Host = " + str?.substring(0, str.length - 1));
+        log("Host = " + str?.substring(0, str.length));
         str = process.env.DB_REFERENCE;
-        log("Ref DB = " + str?.substring(0, str.length - 1));
+        log("Ref DB = " + str?.substring(0, str.length));
         str = process.env.DB_TARGET;
-        log("Target DB = " + str?.substring(0, str.length - 1));
+        log("Target DB = " + str?.substring(0, str.length));
     } catch (error) {
         log("Failed with error " + error);
     }
