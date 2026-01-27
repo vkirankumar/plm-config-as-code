13abc6f65ee8cc66b41d660156e6e135cbb418bf
vkirankumar <rockiran.2525@gmail.com>
2026-01-27 05:43:02 +0100

    Added express, docker and helm.

---
+++ src/express/index.ts
@@ -0,0 +1,24 @@
+import { log } from "console";
+import express from "express";
+
+const app = express();
+const port: number = 3000;
+
+app.listen(port);
+
+log("Node application started at port " + port);
+
+app.get("/health", (req, res) => {
+    res.send("OK");
+    res.status(200);
+});
+
+app.get("/", (req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.send(`
+        <html>
+            <body>
+                <h1>My React App!!</h1>
+            </body>
+        </html>`);
+});
